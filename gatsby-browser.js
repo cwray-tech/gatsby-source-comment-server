de43c500dc820a87ec745a7f8a8f996b4bd71711
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-12-01 07:21:20 -0700

    add css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,150 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-// You can delete this file if you're not using it
+import "./styles.css";
+
+function createElement(name, className, html = null) {
+  const element = document.createElement(name);
+  element.className = className;
+  element.innerHTML = html;
+  return element;
+}
+
+const getCommentsForPage = async (slug) => {
+  const path = slug
+    .split("/")
+    .filter((s) => s)
+    .join("/");
+
+  const comments = await fetch(`/comments/${path}.json`);
+  return comments.json();
+};
+
+const getCommentListItem = (comment) => {
+  const li = createElement("li");
+  li.className = "comment-list-item";
+
+  const nameCont = createElement("div");
+  const name = createElement("strong", "comment-author", comment.name);
+  const date = createElement(
+    "span",
+    "comment-date",
+    new Date(comment.createdAt).toLocaleDateString()
+  );
+
+  nameCont.append(name);
+  nameCont.append(date);
+
+  const commentCont = createElement("div", "comment-cont", comment.content);
+
+  li.append(nameCont);
+  li.append(commentCont);
+  return li;
+};
+
+const createCommentForm = () => {
+  const form = createElement("form");
+  form.className = "comment-form";
+  const nameInput = createElement("input", "name-input", null);
+  nameInput.type = text;
+  nameInput.placehoder = "Your Name";
+  form.appendChild(nameInput);
+
+  const commentInput = createElement("textarea", "comment-input", null);
+  commentInput.placehoder = "Comment";
+  form.appendChild(commentInput);
+
+  const feedback = createElement("span", "feedback");
+  form.appendChild(feedback);
+
+  const button = createElement("button", "comment-btn", "Submit");
+  button.type = "Submit";
+  form.appendChild(button);
+
+  return form;
+};
+
+const updateFeedback = (message = "", className) => {
+  const feedback = document.querySelector(".feedback");
+  feedback.className = `feedback ${className ? className : ""}`.trim();
+  feedback.innerHTML = message;
+
+  return feedback;
+};
+
+exports.onRouteUpdate = async ({ location, prevLocation }, pluginOptions) => {
+  const commentContainer = document.getElementById("commentContainer");
+  if (commentContainer && location.path !== "/") {
+    const header = createElement("h2");
+    header.innerHTML = "Comments";
+    commentContainer.appendChild(header);
+
+    const commentListUl = createElement("ul");
+    commentListUl.className = "comment-list";
+    commentContainer.appendChild(commentListUl);
+
+    const comments = await getCommentsForPage(location.pathname);
+
+    if (comments && comments.length) {
+      comments.map((comment) => {
+        const html = getCommentListItem(comment);
+        commentListUl.append(html);
+        return comment;
+      });
+    }
+
+    commentContainer.appendChild(createCommentForm);
+  }
+
+  document
+    .querySelector("body .comment-form")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault();
+      updateFeedback();
+      const name = document.querySelector(".name-input").value;
+      const comment = document.querySelector(".comment-input").value;
+
+      if (!name) {
+        return updateFeedback("Name is required");
+      }
+      if (!comment) {
+        return updateFeedback("Comment is required");
+      }
+      updateFeedback("Saving comment...", "info");
+
+      const button = document.querySelector(".comment-btn");
+      button.disabled = true;
+
+      const commentData = {
+        name,
+        content: comment,
+        slug: location.pathname,
+        website: pluginOptions.website,
+      };
+
+      fetch(
+        "https://cors-anywhere.herokuapp.com/gatsbyjs-comment-server.herokuapp.com/comments",
+        {
+          body: JSON.stringify(commentData),
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then(async function (result) {
+          const json = await result.json();
+          button.disabled = false;
+
+          if (!result.ok) {
+            updateFeedback(json.error.msg, "error");
+          } else {
+            document.querySelector(".name-input").value = "";
+            document.querySelector(".comment-input").value = "";
+            updateFeedback("Comment has been saved!", "success");
+          }
+        })
+        .catch(async (error) => {
+          const errorText = await error.text();
+          updateFeedback(errorText, "error");
+        });
+    });
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,7 @@
+const axios = require("axios");
+import fs from "fs"
+import {resolve: pathResolve} from "path"
+
 exports.sourceNodes = async ({ actions }) => {
   const { createTypes } = actions;
   const typeDefs = `
@@ -13,4 +17,136 @@ exports.sourceNodes = async ({ actions }) => {
   }
   `;
   createTypes(typeDefs);
+
+  const { createNode } = actions;
+  const { limit, website } = pluginOptions;
+  const _website = website || "";
+
+  const result = await axios({
+    url: `https://Gatsbyjs-comment-server.herokuapp.com/comments?limit=${_limit}&website=${_website}`,
+  });
+
+  const comments = result.data;
+
+  function convertCommentToNode(comment, { createContentDigest, createNode }) {
+    const nodeContent = JSON.stringify(comment);
+
+    const nodeMeta = {
+      id: createNodeId(`comments-${comment._id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: `CommentServer`,
+        mediaType: `text/html`,
+        content: nodeContent,
+        contentDigest: createContentDigest(comment),
+      },
+    };
+
+    const node = Object.assign({}, comment, nodeMeta);
+    createNode(node);
+  }
+
+  for (let i = 0; i < comments.data.length; i++) {
+    const comment = comments.data[i];
+    convertCommentToNode(comment, { createNode, createContentDigest });
+  }
 };
+
+exports.createResolvers = ({ createResolvers }) => {
+  const resolvers = {
+    MarkdownRemark: {
+      comments: {
+        type: ["CommentServer"],
+        resolve(source, args, context, info) {
+          return context.nodeModel.runQuery({
+            query: {
+              filter: {
+                slug: { eq: source.fields.slug },
+              },
+            },
+            type: "CommentServer",
+            firstOnly: false,
+          });
+        },
+      },
+    },
+  };
+  createResolvers(resolvers);
+};
+
+
+
+exports.createPagesStatefully = async ({ graphql }) => {
+  const comments = await graphql(
+    `
+      {
+        allCommentServer(limit: 1000) {
+          edges {
+            node {
+              name
+              slug
+              _id
+              createdAt
+              content
+            }
+          }
+        }
+      }
+    `
+  )
+
+  if (comments.errors) {
+    throw comments.errors
+  }
+
+  const markdownPosts = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const posts = markdownPosts.data.allMarkdownRemark.edges
+  const _comments = comments.data.allCommentServer.edges
+
+  const commentsPublicPath = pathResolve(process.cwd(), "public/comments")
+
+  var exists = fs.existsSync(commentsPublicPath) //create destination directory if it doesn't exist
+
+  if (!exists) {
+    fs.mkdirSync(commentsPublicPath)
+  }
+
+  posts.forEach((post, index) => {
+    const path = post.node.fields.slug
+    const commentsForPost = _comments
+      .filter(comment => {
+        return comment.node.slug === path
+      })
+      .map(comment => comment.node)
+
+    const strippedPath = path
+      .split("/")
+      .filter(s => s)
+      .join("/")
+    const _commentPath = pathResolve(
+      process.cwd(),
+      "public/comments",
+      `${strippedPath}.json`
+    )
+    fs.writeFileSync(_commentPath, JSON.stringify(commentsForPost))
+  })
+}
